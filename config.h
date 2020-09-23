0ddbf40b6ddcf3da1c5591fccdb3ec7d23a5de42
Peter / Talla83 <58005118+talla83@users.noreply.github.com>
2020-09-23 17:49:16 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /*
 
-  ColdEND v1.2 Minimum Quantity Lubrication
+  ColdEND v1.4 Minimum Quantity Lubrication
   https://www.end-cnc-shop.de/geloetetes/3/pumpen-steuerung-1.5-bauteile-set
 
   Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
@@ -10,9 +10,9 @@
   Daniel Seoane / SeoGeo
 
   Rewritten by Tilman, 2020-09-08
-  Rewritten by Talla83.de, 2020-09-21
+  New motor control, fast screen refresh rate and other features added by Talla83.de, 2020-09-21
 
-  Last edited: 2020-09-21
+  Last edited: 2020-09-23
 
 */
 
@@ -31,37 +31,30 @@
 #define outSpitLED 12                       // Spit LED pin
 
 
-// New motor control !!!                    // added Talla83.de
-#include <TimerOne.h>
-// New motor control !!!                    // added Talla83.de
-
 // Mist and spit
-
-// max_flow_rate (max value = 1900 / min value = 500) 
-// value 1900 = 0,40 to 61 rpm (1/16 microstepping)
-// value 1000 = 0,48 to 74 rpm (1/16 microstepping)
-// value  500 = 0,96 to 148 rpm (1/16 microstepping)
-
-#define max_flow_rate  500UL                  // Maximum coolant flow rate (see table above)
-#define fast_flow_rate 80                   // Fast mode flow rate value 80= 115rpm /125=74rpm / 250=37rpm
-#define spit_flow_rate 80                   // Spit mode flow rate value 80= 115rpm /125=74rpm / 250=37rpm
+// max_flow_rate 250 = 37,0 rpm (1/16 microstepping)
+// max_flow_rate 500 = 18,5 rpm (1/16 microstepping)
+// max_flow_rate 1000 = 9,25 rpm (1/16 microstepping)
+// max_flow_rate 3125 = 3,00 rpm (1/16 microstepping)
+#define max_flow_rate 500                 // Maximum coolant flow rate (see table above)
+#define fast_flow_rate 125                  // Fast mode flow rate value 100= 93,75rpm /125=74rpm / 250=37rpm
+#define spit_flow_rate 125                  // Spit mode flow rate value 100= 93,75rpm /125=74rpm / 250=37rpm
 #define spit_min_time 1000                  // Spit mode minimum time in milliseconds
 #define spit_max_time 8000                  // Spit mode maximum time in milliseconds
 #define min_ml_per_hour 1                   // Minimum milliliter per hour
-#define max_ml_per_hour 150                 // Maximum milliliter per hour (needs to be metered before)
+#define max_ml_per_hour 250                 // Maximum milliliter per hour (needs to be metered before)
 
 
 // Operator control
 #define switch_debounce 30                  // Delay time in milliseconds to debounce switch
-//#define linear_control                      // Choose between linear and exponential flow control
+// #define linear_control                      // Choose between linear and exponential flow control
 #define smooth_filter 0.05                  // Exponential filter to smooth potentiometer values
 #define screen_delay 100                    // Minimum delay between two screen refreshes in milliseconds
 
 
 // Display
-#define SSD1306                             // Uncomment for OLED with SSD1306 controller
+ #define SSD1306                             // Uncomment for OLED with SSD1306 controller
 //#define SH1106                              // Uncomment for OLED with SH1106 controller
-
 #define SCREEN_WIDTH 128                    // OLED display width in pixels
 #define SCREEN_HEIGHT 64                    // OLED display height in pixels
 #define OLED_RESET     4                    // Reset pin # (or -1 if sharing Arduino reset pin)
@@ -69,8 +62,8 @@
 #ifdef SSD1306
   #define oled
   #include <Wire.h>
-  #include <Adafruit_GFX.h>
-  #include <Adafruit_SSD1306.h>
+  #include <Adafruit_GFX.h>                 // Required library: https://github.com/adafruit/Adafruit-GFX-Library
+  #include <Adafruit_SSD1306.h>             // Required library: https://github.com/adafruit/Adafruit_SSD1306
   #include <Fonts/FreeSans18pt7b.h>
   Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #endif
@@ -78,8 +71,8 @@
 #ifdef SH1106
   #define oled
   #include <Wire.h>
-  #include <Adafruit_GFX.h>
-  #include <Adafruit_SH1106.h>
+  #include <Adafruit_GFX.h>                 // Required library: https://github.com/adafruit/Adafruit-GFX-Library
+  #include <Adafruit_SH1106.h>              // Required library: https://github.com/wonho-maker/Adafruit_SH1106
   #include <Fonts/FreeSans18pt7b.h>
   Adafruit_SH1106 display(OLED_RESET);
 #endif
